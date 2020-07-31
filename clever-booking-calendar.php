1435a0e2659fdeb0344bc32eab8839c433a66751
Ben Sutton <ben.sutton@craneinteractive.co.uk>
2020-07-31 10:14:17 +0100

    Initial commit

---
+++ clever-booking-calendar.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ * @link              https://github.com/craneinteractive/clever-booking-calendar
+ * @since             0.1.0
+ * @package           CleverBookingCalendar
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Clever Booking Calendar
+ * Plugin URI:        https://github.com/craneinteractive/clever-booking-calendar
+ * Description:       A clever booking calendar system for WordPress
+ * Version:           0.1.0
+ * Author:            Crane Interactive
+ * Author URI:        https://www.craneinteractive.co.uk
+ * License:           GPL-2.0-or-later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       cleverbookingcalendar
+ * Domain Path:       /languages
+ */
